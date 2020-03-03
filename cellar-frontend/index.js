367a3cad724759520f9ca804d64e256658ed8268
enteraether <thetheresagarcia@gmail.com>
2020-03-03 11:42:24 -0500

    stuff

--- cellar-frontend/index.js
+++ cellar-frontend/index.js
@@ -49,17 +49,15 @@ function filterWines(wines){
 //// i added a button to the wine boxes so we can have an event listener for it to show that specific wines details //////
 function loadWines(filteredwines){
    filteredwines.forEach(wine=>{   
-   const wineCard = document.createElement("div")
-
-   wineCard.className = "wine-card"
-   wineCard.dataset.id = wine.id
-      
+      // console.log(wine.id)
+   let wineCard = document.createElement("div")
+   wineCard.className = "wine-card"     
    wineCard.innerHTML = `
    <h4 id="wine-label">${wine.label}</h4>
    <span><em>${wine.region}</em>, ${wine.vintage}</span>
    <br>
    <img src="${wine.image}" class="wine-image"/>
-   <button class='wine-btn' id='${wine.id}'
+   <button class='wine-btn' data-id='${wine.id}'
       <ul>
          ${wine.grapeVariety}
          <br>
@@ -71,23 +69,72 @@ function loadWines(filteredwines){
       </ul>
    </button>
    `
-   wineCardContainer.append(wineCard)
-   
+   wineCardContainer.append(wineCard) 
    let wineButton = document.getElementsByClassName('wine-btn')[0]
-  
-   wineButton.addEventListener('click', (e) => loadIndividualWine(e, wineCard))
+   // debugger
+   wineButton.addEventListener('click', (e) => loadIndividualWine(e, `${wine}`))
 })
-
-/// for some reason i am having trouble figuring out how to set the wineCard button to a variable so i can say wine_card_box.addEventListener instead of document.addEventListener
-
-
 } /// end of loadWines
 
-function loadIndividualWine(e, wineCard){
+function loadIndividualWine(e, wine){
    debugger
-
+   // console.log(e.target.id)
+   fetch(WINESURL + `/${e.target.id}`)
+   .then(resp=>resp.json())
+   .then(individWinePage)
 }
 
+function individWinePage(wine){
+//debugger
+   wineCardContainer.innerHTML = ``
+   const winePage = document.createElement("div")
+   winePage.dataset.id = `${wine.id}`
+   winePage.innerHTML = `
+
+   <main class="wine-page">
+
+   <!-- Left Column / Wine Image -->
+      <div class="left-column">
+         <img data-image="black" src="${wine.image}" alt="">
+      </div>
+
+      <!-- Right Column -->
+      <div class="right-column">
+
+      <!-- Product Description -->
+      <div class="product-description">
+        <h1>${wine.label}</h1>
+        <h3>Notes:</h3>
+        <p>${wine.notes}</p>
+      </div>
+
+      <!-- Product Pricing -->
+      <div class="product-price">
+        <span>$${wine.price}.00</span>
+        <a href="#" class="cart-btn">Add to cart</a>
+      </div><br><br>
+
+      <div class="review">
+         <button class='review-btn'>Add Review</button>
+      </div><br><br>
+
+
+
+      <div class="back-from-product-page">
+         <a href="#" class="back-btn">Back to ${wine.wineType} Wines</a>
+      </div>
+
+    </div>
+
+   </main>
+   `
+   winePage.addEventListener('click', (e)=> addReview(e, wine))
+   wineCardContainer.append(winePage)
+}
+
+function addReview(e, wine){
+
+}
 
 
 
